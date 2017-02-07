07bf18b3cd23d6adeb43f7ffeb01048015386797
H.T. Koo <h1017760@connect.hku.hk>
2017-02-07 22:27:44 +0800

    hea-weather-server: updated package.json to add building step for swagger-ui; updated index page to swagger-ui to disclose api

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     'use strict';
-    res.render('index', {title: 'Express'});
+    res.render('index', {title: 'hey-weather-server'});
 });
 
 module.exports = router;

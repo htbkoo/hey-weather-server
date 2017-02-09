aade94ffdac8a5e11933b650381119d024e54704
H.T. Koo <h1017760@connect.hku.hk>
2017-02-10 07:47:32 +0800

    Heroku: hey-weather-server: /weather/byLatLon - refactored test

--- routes/weather/test/routes.Test.js
+++ routes/weather/test/routes.Test.js
@@ -4,54 +4,71 @@
 
 var Test = require('chai');
 var format = require('string-format');
+var sinon = require('sinon');
 
 var routes = require('../routes');
+var httpFetcher = require('../httpFetcher');
 
 describe("hey-weather-server", function () {
     "use strict";
     describe("Routes for weather", function () {
-        it("should get weather info without callback by lat,lon for '/byLatLon'", function (done) {
-            // Given
+        describe("/byLatLon", function () {
             var path = '/byLatLon';
-            Test.expect(path in routes).to.equal(true, format("Path '{}' should exist", path));
-            var route_root = routes['/byLatLon'];
-            Test.expect(route_root.method).to.equal('get', format("Path '{}' should support get request", path));
+            it("should check existence for '/byLatLon'", function () {
+                // Given
 
-            // When
-            var mock_req = {
-                query: {
-                    "lat": 35,
-                    "lon": 139
-                }
-            };
-            var mock_res = {
-                send: function (message) {
-                    // Then
-                    var usage_response_message =
-                        JSON.stringify({
-                            "coord": {"lon": 139, "lat": 35},
-                            "sys": {"country": "JP", "sunrise": 1369769524, "sunset": 1369821049},
-                            "weather": [{
-                                "id": 804,
-                                "main": "clouds",
-                                "description": "overcast clouds",
-                                "icon": "04n"
-                            }],
-                            "main": {
-                                "temp": 289.5,
-                                "humidity": 89,
-                                "pressure": 1013,
-                                "temp_min": 287.04,
-                                "temp_max": 292.04
-                            },
-                            "wind": {"speed": 7.31, "deg": 187.002},
-                            "rain": {"3h": 0},
-                            "clouds": {"all": 92},
-                            "dt": 1369824698,
-                            "id": 1851632,
-                            "name": "Shuzenji",
-                            "cod": 200
-                        });
+                // When
+
+                // Then
+                Test.expect(path in routes).to.equal(true, format("Path '{}' should exist", path));
+                var route_root = routes[path];
+                Test.expect(route_root.method).to.equal('get', format("Path '{}' should support get request", path));
+
+            });
+
+            it("should get weather info without callback by lat,lon for '/byLatLon'", function (done) {
+                // Given
+                var route_root = routes[path];
+                var mock_req = {
+                    query: {
+                        "lat": 35,
+                        "lon": 139
+                    }
+                };
+                var mock_res = {
+                    send: sendAndAssert
+                };
+                var usage_response_message =
+                    JSON.stringify({
+                        "coord": {"lon": 139, "lat": 35},
+                        "sys": {"country": "JP", "sunrise": 1369769524, "sunset": 1369821049},
+                        "weather": [{
+                            "id": 804,
+                            "main": "clouds",
+                            "description": "overcast clouds",
+                            "icon": "04n"
+                        }],
+                        "main": {
+                            "temp": 289.5,
+                            "humidity": 89,
+                            "pressure": 1013,
+                            "temp_min": 287.04,
+                            "temp_max": 292.04
+                        },
+                        "wind": {"speed": 7.31, "deg": 187.002},
+                        "rain": {"3h": 0},
+                        "clouds": {"all": 92},
+                        "dt": 1369824698,
+                        "id": 1851632,
+                        "name": "Shuzenji",
+                        "cod": 200
+                    });
+
+                // When
+                route_root.fn(mock_req, mock_res);
+
+                // Then
+                function sendAndAssert(message) {
                     Test.expect(message).to.equal(JSON.stringify(
                         {
                             "lat": 35,
@@ -60,9 +77,7 @@ describe("hey-weather-server", function () {
                     ));
                     done();
                 }
-            };
-            route_root.fn(mock_req, mock_res);
-
+            });
         });
     });
 });
